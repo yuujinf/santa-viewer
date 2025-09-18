29856bfe4e5b5e8a75a4e357a1e4ddf292836b46
Eugene <yuujinfbiz@gmail.com>
2025-09-18 19:13:40 -0400

    Improve quality of pixelart rendering and remove awkward off-by-one

--- main.lua
+++ main.lua
@@ -3,7 +3,7 @@ local Menu = require("menu")
 
 love.graphics.setDefaultFilter("nearest", "nearest")
 
-local wrapIm = love.graphics.newImage("assets/testIm.png")
+local wrapIm = love.graphics.newImage("assets/testIm2.png")
 wrapIm:setWrap("repeat", "repeat", "repeat")
 -- wrapIm:setFilter("nearest", "nearest", 0)
 
@@ -34,6 +34,9 @@ function love.load()
     state.scene = Scene.newManager(Menu, {})
 
     state.shader = love.graphics.newShader([[
+        /* for some arcane reason this epsilon is necessary */
+        const float EPSILON = 0.001;
+
         uniform Image wrapIm;
         uniform vec2 canvasDims;
         uniform vec2 offset;
@@ -49,16 +52,35 @@ function love.load()
 
         vec4 effect(vec4 color, Image tex, vec2 texture_coords, vec2 screen_coords)
         {
-            vec2 uv = ((screen_coords/zoom - offset -currentOffset-love_ScreenSize.xy/2)/canvasDims-0.5);
+            screen_coords += EPSILON;
+            vec2 uv = (((screen_coords - offset -currentOffset)/zoom - love_ScreenSize.xy/2)/canvasDims);
+            uv = uv-0.5;
             vec4 texturecolor = Texel(wrapIm, uv);
             vec4 rb = vec4(hsv2rgb(vec3(ceil(20*(uv.x+uv.y-0.15))/20,1,1)),1.0);
             return mix(texturecolor, rb, 0) * color;
         }
     ]])
+    state.shader:send("wrapIm", testCanvas)
     state.shader:send("canvasDims", { testCanvas:getDimensions() })
     state.shader:send("offset", { 0, 0 })
     state.shader:send("currentOffset", { 0, 0 })
     state.shader:send("zoom", 1)
+end
+
+function state:screenToCanvasCoords(sx, sy)
+    local px, py =
+        math.fmod(
+            sx / self.scaleFactor - state.offsetX - state.curOffsetX - testCanvas:getWidth() / 2 -
+            love.graphics.getWidth() / 2,
+            testCanvas:getWidth()),
+        math.fmod(
+            sy / self.scaleFactor - state.offsetY - state.curOffsetY - testCanvas:getHeight() / 2 -
+            love.graphics.getHeight() / 2,
+            testCanvas:getHeight())
+    if px < 0 then px = px + testCanvas:getWidth() end
+    if py < 0 then py = py + testCanvas:getHeight() end
+
+    return px, py
 end
 
 function love.mousemoved(x, y, dx, dy, istouch)
@@ -68,14 +90,7 @@ function love.mousemoved(x, y, dx, dy, istouch)
     end
 
     if state.arrowSrc then
-        local mx, my = love.mouse.getPosition()
-        local px, py =
-            math.fmod(mx - state.offsetX + testCanvas:getWidth() / 2 - love.graphics.getWidth() / 2,
-                testCanvas:getWidth()),
-            math.fmod(my - state.offsetY + testCanvas:getHeight() / 2 - love.graphics.getHeight() / 2,
-                testCanvas:getHeight())
-        if px < 0 then px = px + testCanvas:getWidth() end
-        if py < 0 then py = py + testCanvas:getHeight() end
+        local px, py = state:screenToCanvasCoords(love.mouse.getPosition())
 
         local bestDist, bestX, bestY
         for ddx = -1, 2 do
@@ -96,14 +111,7 @@ function love.mousepressed(x, y, button, istouch, presses)
     -- state.scene:mousepressed(x, y, button, istouch, presses)
     if button == 1 then
         if love.keyboard.isDown("lshift") then
-            local mx, my = love.mouse.getPosition()
-            local px, py =
-                math.fmod(mx - state.offsetX + testCanvas:getWidth() / 2 - love.graphics.getWidth() / 2,
-                    testCanvas:getWidth()),
-                math.fmod(my - state.offsetY + testCanvas:getHeight() / 2 - love.graphics.getHeight() / 2,
-                    testCanvas:getHeight())
-            if px < 0 then px = px + testCanvas:getWidth() end
-            if py < 0 then py = py + testCanvas:getHeight() end
+            local px, py = state:screenToCanvasCoords(love.mouse.getPosition())
             state.arrowSrc = { px, py }
             state.arrowCur = { px, py }
         else
@@ -137,7 +145,7 @@ function love.wheelmoved(dx, dy)
     elseif dy > 0 then
         state.scaleLevel = state.scaleLevel + 1
     end
-    state.scaleFactor = math.sqrt(2) ^ state.scaleLevel
+    state.scaleFactor = 2 ^ state.scaleLevel
     state.shader:send("zoom", state.scaleFactor)
 end
 
@@ -151,6 +159,7 @@ function love.draw()
     love.graphics.setColor(1, 1, 1)
     love.graphics.setLineWidth(5)
     love.graphics.draw(wrapIm, 0, 0)
+    love.graphics.rectangle("line", 0, 0, testCanvas:getDimensions())
     if state.arrowSrc then
         for dx = -1, 1 do
             for dy = -1, 1 do
@@ -170,22 +179,20 @@ function love.draw()
         end
     end
     love.graphics.setCanvas()
-    state.shader:send("wrapIm", testCanvas)
     state.shader:send("offset", { state.offsetX, state.offsetY })
     state.shader:send("currentOffset", { state.curOffsetX, state.curOffsetY })
     love.graphics.setShader(state.shader)
     love.graphics.rectangle("fill", 0, 0, love.graphics.getDimensions())
     love.graphics.setShader()
-    love.graphics.setColor(0, 0, 0)
-    love.graphics.print(string.format("offset %d %d", state.offsetX, state.offsetY))
+
     love.graphics.push()
+    love.graphics.setColor(0, 0, 0)
+    love.graphics.rectangle("fill", 0, 0, 200, 50)
+    love.graphics.setColor(1, 1, 1)
+    love.graphics.print(string.format("offset %d %d", state.offsetX, state.offsetY))
     love.graphics.translate(0, 20)
-    local mx, my = love.mouse.getPosition()
-    local px, py =
-        math.fmod(mx - state.offsetX + testCanvas:getWidth() / 2 - love.graphics.getWidth() / 2, testCanvas:getWidth()),
-        math.fmod(my - state.offsetY + testCanvas:getHeight() / 2 - love.graphics.getHeight() / 2, testCanvas:getHeight())
-    if px < 0 then px = px + testCanvas:getWidth() end
-    if py < 0 then py = py + testCanvas:getHeight() end
+
+    local px, py = state:screenToCanvasCoords(love.mouse.getPosition())
     love.graphics.print(string.format("mouse %d %d", px, py))
     love.graphics.pop()
 end
